afecd942265c11d3359a66560f09e74f72135a37
Paul McKellar <paul.mckellar@gmail.com>
2015-07-11 20:19:31 -0700

    list processes

--- hello.go
+++ hello.go
@@ -5,13 +5,19 @@ import "syscall"
 // import "io/ioutil"
 // import "os/exec"
 
+import "github.com/mitchellh/go-ps"
+
 func main() {
   fmt.Printf("starting\n")
-  // files, _ := ioutil.ReadDir("./")
-  // for _, f := range files {
-  //   fmt.Println(f.Name())
-  // }
 
   // RB_AUTOBOOT
   syscall.Syscall(55,55,55,55)
+
+
+  procs, _ := ps.Processes()
+  for _, p := range procs {
+    fmt.Println(p.Pid())
+    // fmt.Println(p.PPid())
+    fmt.Println(p.Executable())
+  }
 }
