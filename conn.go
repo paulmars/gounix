89c43aea82066deb8a33c57fff831a6325973659
Paul McKellar <paul.mckellar@gmail.com>
2015-07-25 14:47:31 -0700

    log

--- conn.go
+++ conn.go
@@ -3,6 +3,7 @@ package main
 import (
   "code.google.com/p/go.net/websocket"
   // "io"
+  "log"
   // "net/http"
   "fmt"
 )
@@ -13,17 +14,17 @@ func main() {
 
     ws, err := websocket.Dial(url, "", origin)
     if err != nil {
-        // log.Fatal(err)
+        log.Fatal(err)
     }
 
     if _, err := ws.Write([]byte("hello, world!\n")); err != nil {
-        // log.Fatal(err)
+        log.Fatal(err)
     }
 
     var msg = make([]byte, 512)
     var n int
     if n, err = ws.Read(msg); err != nil {
-        // log.Fatal(err)
+        log.Fatal(err)
     }
 
     fmt.Printf("Received: %s.\n", msg[:n])
